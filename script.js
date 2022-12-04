03d6c456a7a35a147b4e4f2078f31f9910f277e3
hamda <hamda.baashe44@gmail.com>
2022-12-04 20:38:30 +0000

    Done the weather-app

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // write code below
+
+document.querySelector(".btn").addEventListener('click', ()=>{
+    let city = document.querySelector("#search").value
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9f162b4ce2c9943b3c6b16f0a9645d25`)
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        document.querySelector("#location").innerHTML = data.name
+       document.querySelector(".desc").innerHTML = data.weather[0].description
+       document.querySelector(".c").innerHTML = data.main.temp
+    })
+})
